0b16d1f69b1ecc6079012ef788a8e66aa3acba68
Abraão Állysson dos Santos Honório <abraao.allyson@eng.ci.ufpb.br>
2018-05-29 16:14:10 -0300

    criando um config para banco

---
+++ src/config/config.js
@@ -0,0 +1,31 @@
+'use strict';
+var express = require('express')
+var router = express.Router()
+var path = require('path')
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: 'postgres://drzbqfbrmgjxxd:175818dc32b01db709ec310418477a11cb357d29e86f730d6371cf29e71d9015@ec2-23-23-130-158.compute-1.amazonaws.com:5432/d8lji6g22tlttp',
+  ssl: true,
+});
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+  });
+  
+  client.connect((err, done) => {
+      if(err){
+          //done()
+          console.log(err)
+          return res.status(500).json({success: false, data: err})
+      }
+  })
+  
+exports.get=  () =>  {
+   
+   
+    return client;   
+}
