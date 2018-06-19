d9ec9d004ab4fc9b6e7f4993300d4eef171035e8
josuepaiva <josuepaiva@lavid.ufpb.br>
2018-06-19 15:13:01 -0300

    Update avaliacaoDesempenhoRepository.js

--- src/repositories/avaliacaoDesempenhoRepository.js
+++ src/repositories/avaliacaoDesempenhoRepository.js
@@ -22,7 +22,7 @@ const client = ImportClient.get();
     console.log('AVALIACAO_DESEMPENHO ' + JSON.stringify(AvaliacaoDesempenho));
 
    
-       client.query('INSERT INTO "AVALIACAO_DESEMPENHO"("dt_Ano", "id_Usuario_Avaliado", "id_Usuario_Avaliador", \
+     const query = client.query('INSERT INTO "AVALIACAO_DESEMPENHO"("dt_Ano", "id_Usuario_Avaliado", "id_Usuario_Avaliador", \
                     "Pergunta_1_1","Pergunta_1_2","Pergunta_1_3","Pergunta_2_1","Pergunta_2_2","Pergunta_2_3","Pergunta_3_1","Pergunta_3_2", \
                     "Pergunta_3_3","Pergunta_4_1","Pergunta_4_2", "Pergunta_4_3","Pergunta_5_1","Pergunta_5_2","Pergunta_5_3")  \
                      values($1,$2, $3, $4, $5, $6,$7, $8, $9,$10,$11, $12, $13,$14,$15, $16, $17,$18)',
@@ -31,9 +31,9 @@ const client = ImportClient.get();
                 AvaliacaoDesempenho.Pergunta_2_1, AvaliacaoDesempenho.Pergunta_2_2, AvaliacaoDesempenho.Pergunta_2_3,
                 AvaliacaoDesempenho.Pergunta_3_1, AvaliacaoDesempenho.Pergunta_3_2, AvaliacaoDesempenho.Pergunta_3_3,
                 AvaliacaoDesempenho.Pergunta_4_1, AvaliacaoDesempenho.Pergunta_4_2, AvaliacaoDesempenho.Pergunta_4_3,
-                AvaliacaoDesempenho.Pergunta_5_1, AvaliacaoDesempenho.Pergunta_5_2, AvaliacaoDesempenho.Pergunta_5_3])
-     
+                AvaliacaoDesempenho.Pergunta_5_1, AvaliacaoDesempenho.Pergunta_5_2, AvaliacaoDesempenho.Pergunta_5_3]);
    
+      return query;
 }
 
 exports.getById =  function(idAvaliacaoDesempenho)  {
